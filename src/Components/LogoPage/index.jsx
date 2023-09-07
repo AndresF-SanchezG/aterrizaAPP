804c46532c35aee03f5cc5e6780659fea26b6735
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-09-07 08:31:03 -0500

    V2

--- src/Components/LogoPage/index.jsx
+++ src/Components/LogoPage/index.jsx
@@ -3,7 +3,7 @@ const LogoPage = () => {
     return(
         <div className="bg-white cursor-pointer w-36 h-36 rounded-lg flex justify-center items-center md:mt-2">
             <figure className='relative mb-2 w-full h-full flex justify-center items-center'>
-                <img className='w-full h-full object-cover rounded-lg w-36 h-36 ' src='/images/picture (3).svg' alt='logoAteriza'/>
+                <img className='w-full h-full object-cover rounded-lg w-36 h-36 ' src='/images/picture(3).svg' alt='logoAteriza'/>
             </figure>
         
         </div>
