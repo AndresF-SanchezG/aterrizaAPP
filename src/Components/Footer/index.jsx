fe4cf871c814b619631363b8d9496aaca2683248
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-09-12 08:34:29 -0500

    V11

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,30 +1,37 @@
 import { NavLink } from "react-router-dom";
+import logotipoInstagram from '../../images/logotipo-de-instagram.png'
+import logoFacebook from '../../images/facebook.png'
+import logoWhatsapp from '../../images/whatsapp.png'
+
 
 const Footer = () => {
     
     
     return (
-        <footer className="bottom-0 flex justify-center items-center z-10 w-full py-5 px-6 text-sm font-light">
-            <ul className="w-full flex flex-col items-center gap-3 ">
-                <li >
-                    <NavLink  to='/instagram' className={({ isActive }) =>
+        <footer className="bottom-0 flex justify-center items-center z-10 w-full py-5 px-6 text-sm font-light md:fixed">
+            <ul className="w-full flex flex-col items-center gap-3 md:flex-row justify-around">
+                <li className=" w-40 flex justify-center items-center">
+                    <NavLink  to='https://www.instagram.com/aterriza.co/' className={({ isActive }) =>
                         isActive ? activeStyle: undefined
                 }>
-                        Instagram
+                    <span className="flex justify-center items-center gap-2"><img className="w-8 h-8 flex" src={logotipoInstagram}/>Instagram</span>
+                       
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/hf' className={({ isActive }) =>
+                <li className=" w-40 flex justify-center items-center">
+                    <NavLink to='https://www.facebook.com/profile.php?id=100064084713525&locale=es_LA' className={({ isActive }) =>
                         isActive ? activeStyle: undefined
                 }>
-                        Facebook
+                    <span className="flex justify-center items-center gap-2"><img className="w-8 h-8 flex" src={logoFacebook}/>Facebook</span>
+                        
                     </NavLink>
                 </li>
-                <li>
-                    <NavLink to='/nnh' className={({ isActive }) =>
+                <li className=" w-40 flex justify-center items-center">
+                    <NavLink to='https://api.whatsapp.com/message/YLPVIZPCJH2JN1?autoload=1&app_absent=0' className={({ isActive }) =>
                         isActive ? activeStyle: undefined
                 }>
-                        Registro Turismo
+                    <span className="flex justify-center items-center gap-2"><img className="w-8 h-8 flex" src={logoWhatsapp}/>Whatsapp</span>
+                        
                     </NavLink>
                 </li>
             
