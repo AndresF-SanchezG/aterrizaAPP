87740771465e297ed32838f32aec35b3b58824de
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-09-12 14:39:42 -0500

    V15

--- src/Components/Formulario/index.jsx
+++ src/Components/Formulario/index.jsx
@@ -1,36 +1,24 @@
 
 const Formulario = () => {
+    const tipoDatosCotizacion = ['Selecciona tu Hotel', 'Selecciona tipo de habitacion', 'Fecha de entrada', 'Fecha de salida', 'Ingresa la cantidad de adultos', 'Ingresa la cantidad de niños'];
     return(
-      <div className="mt-4 m-autoflex justify-center items-center border border-blue-700 rounded-2xl px-4 mb-2">
+ 
+    <div className="mt-4 m-autoflex justify-center items-center border border-blue-700 rounded-2xl px-4 mb-2">
         <p className="text-blue-700 flex font-bold text-sm justify-center mt-4">REALIZA TU COTIZACION RAPIDAMENTE</p>
-        <form>
-            <ul className="py-4 space-y-8 ">
-              
-                <li className="border border-blue-700 flex justify-center">
-                    <select />
-                    <option>Selecciona tu Hotel</option>
-                </li>
-                <li className="border border-blue-700 flex justify-center">
-                    <select />
-                    <option>Selecciona tipo de habitacion</option>
-                </li>
-                <li className="border border-blue-700 flex justify-center">
-                    <select />
-                    <option>Selecciona tipo de habitación</option>
-                </li>
-                <li className="border border-blue-700 flex justify-center">
-                    <select />
-                    <option>Ingresa la cantidad de adultos</option>
-                </li>
-                <li className="border border-blue-700 flex justify-center">
-                    <select />
-                    <option>Ingresa la cantidad de niños</option>
-                </li>
-            </ul>
+        {tipoDatosCotizacion?.map((dato) => (
+             <form>
+             <ul className="py-4 space-y-8 ">
+               
+                 <li className="border border-blue-700 flex justify-center">
+                     <select>
+                     <option>{dato}</option>
+                     </select>
+                 </li>
+                </ul>
+            </form>
 
-        </form>
-
-      </div>
+        ))}
+    </div>
     )
 }
 
