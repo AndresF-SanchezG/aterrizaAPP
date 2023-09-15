8df2cb08800723a9a816f8db35dccbce3127973f
AndresF-sanchezG <e.digitalcolombia@gmail.com>
2023-09-15 15:07:55 -0500

    V21

--- src/Components/Formulario/index.jsx
+++ src/Components/Formulario/index.jsx
@@ -1,23 +1,29 @@
 
 const Formulario = () => {
-    const tipoDatosCotizacion = ['Selecciona tu Hotel', 'Selecciona tipo de habitacion', 'Fecha de entrada', 'Fecha de salida', 'Ingresa la cantidad de adultos', 'Ingresa la cantidad de niños'];
+    const tipoDatosCotizacion = ['Ciudad','Selecciona tu Hotel', 'Selecciona tipo de habitacion', 'Fecha de entrada', 'Fecha de salida', 'Ingresa la cantidad de adultos', 'Ingresa la cantidad de niños'];
     return(
  
-    <div className=" m-autoflex justify-center items-center border border-purple-800 border-2 rounded-2xl px-4 mb-12">
+    <div className=" m-auto flex flex-col justify-center items-center border border-purple-800 border-2 rounded-2xl px-4 mb-12">
         <p className="text-purple-800 flex font-bold text-sm justify-center mt-4">REALIZA TU COTIZACION RAPIDAMENTE</p>
-        {tipoDatosCotizacion?.map((dato) => (
+  
              <form>
+           
              <ul className="py-4 space-y-8 ">
-               
+             {tipoDatosCotizacion?.map((dato) => (
                  <li className="border border-purple-800 border-2 flex justify-center">
                      <select className="w-full">
                      <option className="w-full">{dato}</option>
+                    
                      </select>
+                    
                  </li>
-                </ul>
+                 ))}
+                  <button className="w-full flex justify-center items-center p-2 bg-purple-800 mb-4 text-white font-bold">COTIZAR</button>
+                </ul> 
             </form>
+          
 
-        ))}
+      
     </div>
     )
 }
